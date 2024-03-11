3c9bcf5ecc8533a49ad099232826add7a69e72df
oyeyinkaojora <oyeyinkaojoro@gmail.com>
2024-03-11 10:19:21 +0100

    Swiper Js integrated for category card swipping

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import { CardsData } from "../components/CardsData";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
 
 const LandingPage = () => {
   return (
@@ -16,25 +21,42 @@ const LandingPage = () => {
         </div>
       </div>
 
-      <div className="container-fluid mt-3">
-        <div className="row mx-md-5">
-          {CardsData.map((card) => (
-            <div key={card.id} className="col-md-2 my-2">
-              <div style={{ borderRadius: "20px" }} className="card shadow-sm">
-                <div className="card-body text-center d-flex flex-column align-items-center justify-content-center">
-                  <img
-                    src={card.imagePath}
-                    alt={card.name}
-                    className="img-fluid mb-2"
-                    style={{ maxWidth: "100px", maxHeight: "100px" }}
-                  />
-                  <span>{card.name}</span>
-                </div>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        loop={true}
+        navigation={true}
+        modules={[Navigation]}
+        autoplay={{ delay: 1000 }}
+        className="mySwiper"
+      >
+        {CardsData.map((card, index) => (
+          <SwiperSlide key={card.id}>
+            <div className="container-fluid mt-3">
+              <div className="row mx-md-5">
+                {CardsData.slice(index, index + 6).map((card) => (
+                  <div key={card.id} className="col-md-2 my-2">
+                    <div
+                      style={{ borderRadius: "20px" }}
+                      className="card shadow-sm"
+                    >
+                      <div className="card-body text-center d-flex flex-column align-items-center justify-content-center">
+                        <img
+                          src={card.imagePath}
+                          alt={card.name}
+                          className="img-fluid mb-2"
+                          style={{ maxWidth: "100px", maxHeight: "100px" }}
+                        />
+                        <span>{card.name}</span>
+                      </div>
+                    </div>
+                  </div>
+                ))}
               </div>
             </div>
-          ))}
-        </div>
-      </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 };
