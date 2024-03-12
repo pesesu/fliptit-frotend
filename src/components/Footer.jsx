e1106e25af4212f6c1349b564d3e42567506a9d7
oyeyinkaojora <oyeyinkaojoro@gmail.com>
2024-03-12 12:17:29 +0100

    Product card fixes

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <div className="container-fluid">
+      <div className=" mx-md-5 d-flex">
+        <div className="">
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
