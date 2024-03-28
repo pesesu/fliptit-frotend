c607a74b59ce891b7be97ae5ca79874a335cc929
Pesesu <stephenogun227@gmail.com>
2024-03-28 07:49:51 +0100

    Redesigned category page

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -16,13 +16,13 @@ const CategoryPage = () => {
         return formattedPrice;
       };
   return (
-    <div style={{ background: "#E9E9E9" }}>
-        <div >
+    <div>
+        <div style={{ background: "#E9E9E9" }}>
             <Navbar />
         </div>
-        <div className="container-fluid mx-md-5 py-3">
-            <h4 class="mt-md-2 mb-4" style={{fontWeight:"700"}}>Electronics</h4>
-            <div className="sub-categories d-flex flex-wrap gap-4 mt-4 mt-md-3">
+        <div className="container-fluid mx-md-5 pt-4 pb-5">
+            <h4 class="mt-md-2 mb-1 pb-2 pb-md-0" style={{fontWeight:"700"}}>Electronics</h4>
+            <div className="sub-categories d-flex flex-wrap gap-3 mt-2 py-md-2">
                 <a href="">Phone & Accessories</a>
                 <a href="">Camera & photograpy</a>
                 <a href="">Computers & Laptops</a>
@@ -30,13 +30,13 @@ const CategoryPage = () => {
                 <a href="">Wearable Technology</a>
             </div>
             {/* <hr  className='my-4 d-lg-none'/> */}
-            <div className="price-filter d-flex gap-2 align-items-center pt-1 pt-lg-0 mt-4 mt-md-3">
-                <div>Price:</div>
+            <div className="price-filter d-flex gap-2 align-items-center pt-2 pt-lg-0 mt-3 mb-1">
+                <div className="mt-2">Price:</div>
                 <div className="price-filter-input-wrapper mt-3">
                   <div className="price-filter-text"><small>Min</small></div>
                   <input class="price-filter-input" id="min-price" type="text" />
                 </div>
-                <div style={{width:7, height:3, background:"#444444"}}  className="filter-sep"></div>
+                <div style={{width:7, height:3, background:"#444444"}}  className="filter-sep mt-3"></div>
                 <div className="price-filter-input-wrapper mt-3">
                   <div className="price-filter-text"><small>Max</small></div>
                     <input class="price-filter-input"  id="max-price" type="text" />
@@ -45,9 +45,9 @@ const CategoryPage = () => {
         </div>
         {/* Products */}
 
-        <div style={{padding: "40px 0 60px" }} className="container-fluid">
+        <div style={{padding: "40px 0 60px", background: "#E9E9E9" }} className="container-fluid">
           <div className="row g-4 mx-1 mx-md-5 align-items-center justify-content-center">
-            <h4 style={{fontWeight:600, lineHeight:0}}><span style={{fontWeight:400}} className='page-cat-nam'>Electronics</span> near you</h4>
+            <h4 style={{fontWeight:600, lineHeight:0}}><span style={{fontWeight:400, color:"#00754B"}} className='page-cat-nam'>Electronics</span> near you</h4>
             {productsData.map((products) => (
               <div key={products.id} className="col-sm-6 col-md-4 col-lg-3 mb-2">
                 <div className="card product-cards shadow-sm">
